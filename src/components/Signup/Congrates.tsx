97417cf15261890bcfb5e062e4843d0fc9534ccb
Lemon-zeniye <endalklemon.abate@gmail.com>
2024-12-30 17:16:27 +0300

    signup pages

---
+++ src/components/Signup/Congrates.tsx
@@ -0,0 +1,35 @@
+import AuthWrapper from "../auth/AuthWrapper";
+import logo from "../../assets/logo/new-logo.svg";
+import congrats from "../../assets/svg/congrats.svg";
+import { Button } from "../ui/button";
+
+function Congrats() {
+  return (
+    <AuthWrapper>
+      <div>
+        <img className="w-52 mx-auto" src={logo} alt="" />
+        <div className="flex flex-col items-center my-4 mt-6">
+          <img className="w-36 mx-auto" src={congrats} alt="" />
+          <div className="text-3xl font-semibold mt-4 text-primary">
+            Congrats
+          </div>
+          <div className="text-center text-sm mt-2 max-w-lg">
+            You have successfully registered on Mindsight!
+          </div>
+          <div className="text-center text-sm mt-2 max-w-lg">
+            We have prepared a minor assessment to help you start right from
+            where you belong.
+          </div>
+          <Button className=" px-10 py-2 mt-6 shadow rounded-3xl bg-primary text-white ">
+            Take Assessment
+          </Button>
+          <Button className=" px-7 mt-1  border-none rounded-3x bg-transparent text-black hover:bg-gray-200 hover:sahdow hover:rounded-3xl ">
+            Skip
+          </Button>
+        </div>
+      </div>
+    </AuthWrapper>
+  );
+}
+
+export default Congrats;
