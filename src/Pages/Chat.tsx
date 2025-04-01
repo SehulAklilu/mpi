23990009766e403e7fc160aa219b4a1bc2e6e72a
Lemon-zeniye <endalklemon.abate@gmail.com>
2025-04-01 09:25:09 +0300

    socket setup

--- src/Pages/Chat.tsx
+++ src/Pages/Chat.tsx
@@ -7,6 +7,7 @@ import SocialFeed from "@/components/Chat/Post";
 import { ContentLayout } from "@/components/Sidebar/contenet-layout";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useRole } from "@/RoleContext";
+import { SocketProvider } from "@/socket/SocketContext";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
@@ -22,74 +23,76 @@ function Chat() {
   }, [chatId]);
   return (
     <ContentLayout>
-      <div className="">
-        <div className="flex items-center justify-center">
-          <Tabs
-            value={activeTab}
-            onValueChange={setActiveTab}
-            defaultValue="messages"
-            className="w-full"
-          >
-            <TabsList className=" hidden md:flex bg-[#FFF6ED] rounded-full w-full md:w-[34rem] lg:w-[40rem] shadow-md h-[2.5rem] md:h-[3rem] mx-auto border">
-              <TabsTrigger
-                value="messages"
-                className="flex-1 text-center py-1 text-sm md:text-base lg:text-lg rounded-full transition-colors data-[state=active]:bg-[#F2851C] data-[state=active]:text-white data-[state=inactive]:text-gray-700"
-              >
-                Messages
-              </TabsTrigger>
-              <TabsTrigger
-                value="group"
-                className="flex-1 text-center py-1 text-sm md:text-base lg:text-lg rounded-full transition-colors data-[state=active]:bg-[#F2851C] data-[state=active]:text-white data-[state=inactive]:text-gray-700"
-              >
-                Groups
-              </TabsTrigger>
-              <TabsTrigger
-                value="people"
-                className="flex-1 text-center py-1 text-sm md:text-base lg:text-lg rounded-full transition-colors data-[state=active]:bg-[#F2851C] data-[state=active]:text-white data-[state=inactive]:text-gray-700"
-              >
-                People
-              </TabsTrigger>
-              <TabsTrigger
-                value="announcements"
-                className="flex-1 text-center py-1 text-sm md:text-base lg:text-lg rounded-full transition-colors data-[state=active]:bg-[#F2851C] data-[state=active]:text-white data-[state=inactive]:text-gray-700"
-              >
-                Announcements
-              </TabsTrigger>
-              <TabsTrigger
-                value="posts"
-                className="flex-1 text-center py-1 text-sm md:text-base lg:text-lg rounded-full transition-colors data-[state=active]:bg-[#F2851C] data-[state=active]:text-white data-[state=inactive]:text-gray-700"
-              >
-                Community
-              </TabsTrigger>
-            </TabsList>
-            <TabsContent className="!mt-0" value="messages">
-              <ChatComponent
-                setActiveTab={setActiveTab}
-                openChatId={openChatId}
-              />
-            </TabsContent>
-            <TabsContent className="!mt-0" value="group">
-              <GroupChat setActiveTab={setActiveTab} />
-            </TabsContent>
-            <TabsContent className="!mt-0" value="people">
-              <PeopleComponent
-                setActiveTab={setActiveTab}
-                setOpenChatId={setOpenChatId}
-              />
-            </TabsContent>
-            <TabsContent className="!mt-0" value="announcements">
-              {role && role === "coach" ? (
-                <Announcements setActiveTab={setActiveTab} />
-              ) : (
-                <AnnouncementsPlayer setActiveTab={setActiveTab} />
-              )}
-            </TabsContent>
-            <TabsContent className="!mt-5" value="posts">
-              <SocialFeed setActiveTab={setActiveTab} />
-            </TabsContent>
-          </Tabs>
+      <SocketProvider>
+        <div className="">
+          <div className="flex items-center justify-center">
+            <Tabs
+              value={activeTab}
+              onValueChange={setActiveTab}
+              defaultValue="messages"
+              className="w-full"
+            >
+              <TabsList className=" hidden md:flex bg-[#FFF6ED] rounded-full w-full md:w-[34rem] lg:w-[40rem] shadow-md h-[2.5rem] md:h-[3rem] mx-auto border">
+                <TabsTrigger
+                  value="messages"
+                  className="flex-1 text-center py-1 text-sm md:text-base lg:text-lg rounded-full transition-colors data-[state=active]:bg-[#F2851C] data-[state=active]:text-white data-[state=inactive]:text-gray-700"
+                >
+                  Messages
+                </TabsTrigger>
+                <TabsTrigger
+                  value="group"
+                  className="flex-1 text-center py-1 text-sm md:text-base lg:text-lg rounded-full transition-colors data-[state=active]:bg-[#F2851C] data-[state=active]:text-white data-[state=inactive]:text-gray-700"
+                >
+                  Groups
+                </TabsTrigger>
+                <TabsTrigger
+                  value="people"
+                  className="flex-1 text-center py-1 text-sm md:text-base lg:text-lg rounded-full transition-colors data-[state=active]:bg-[#F2851C] data-[state=active]:text-white data-[state=inactive]:text-gray-700"
+                >
+                  People
+                </TabsTrigger>
+                <TabsTrigger
+                  value="announcements"
+                  className="flex-1 text-center py-1 text-sm md:text-base lg:text-lg rounded-full transition-colors data-[state=active]:bg-[#F2851C] data-[state=active]:text-white data-[state=inactive]:text-gray-700"
+                >
+                  Announcements
+                </TabsTrigger>
+                <TabsTrigger
+                  value="posts"
+                  className="flex-1 text-center py-1 text-sm md:text-base lg:text-lg rounded-full transition-colors data-[state=active]:bg-[#F2851C] data-[state=active]:text-white data-[state=inactive]:text-gray-700"
+                >
+                  Community
+                </TabsTrigger>
+              </TabsList>
+              <TabsContent className="!mt-0" value="messages">
+                <ChatComponent
+                  setActiveTab={setActiveTab}
+                  openChatId={openChatId}
+                />
+              </TabsContent>
+              <TabsContent className="!mt-0" value="group">
+                <GroupChat setActiveTab={setActiveTab} />
+              </TabsContent>
+              <TabsContent className="!mt-0" value="people">
+                <PeopleComponent
+                  setActiveTab={setActiveTab}
+                  setOpenChatId={setOpenChatId}
+                />
+              </TabsContent>
+              <TabsContent className="!mt-0" value="announcements">
+                {role && role === "coach" ? (
+                  <Announcements setActiveTab={setActiveTab} />
+                ) : (
+                  <AnnouncementsPlayer setActiveTab={setActiveTab} />
+                )}
+              </TabsContent>
+              <TabsContent className="!mt-5" value="posts">
+                <SocialFeed setActiveTab={setActiveTab} />
+              </TabsContent>
+            </Tabs>
+          </div>
         </div>
-      </div>
+      </SocketProvider>
     </ContentLayout>
   );
 }
